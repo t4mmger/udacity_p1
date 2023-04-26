1d478d7d31618461a6e7566827297a3338752a3d
t4mmger <tobias-ammon@gmx.de>
2023-04-26 22:34:09 +0200

    erste_analysen

--- udacity_p1_code.py
+++ udacity_p1_code.py
@@ -21,25 +21,21 @@ while True:
 headers = {'White': [], 'WhiteElo': [], 'Black': [], 'BlackElo': [], 'Result': [], 'ECO': [], 'PlyCount': [], 'Date': []}
 pieces = {'Pieces': []}
 for i, g in enumerate(my_games):
-    #print(i)
+
     # this collects the headers information
     for el in headers: 
-        #print(el)
-        #print(g.headers[el])
         headers[el] += [g.headers[el]]
     
     # this collects the number of pieces at the end of the game
     pieces['Pieces'] += [32 - str(g.mainline_moves()).count('x')]
-    if i >= 2:
+    if i >= 5:
         break
 
 chess_df = {**headers,**pieces}
 
-#print(my_games[0].mainline_moves())
-
 df = pd.DataFrame(chess_df)
 
-df[['WhiteElo','BlackElo']] = df[['WhiteElo', 'BlackElo']].apply(pd.to_numeric)
+df[['WhiteElo','BlackElo','Pieces','PlyCount']] = df[['WhiteElo', 'BlackElo','Pieces','PlyCount']].apply(pd.to_numeric)
 
 
 
@@ -60,18 +56,27 @@ df[['WhiteElo','BlackElo']] = df[['WhiteElo', 'BlackElo']].apply(pd.to_numeric)
 #df['Probability'] = df.apply(apply_elo_prob, axis = 1)    
 
 
-
 #formula for probability from https://fivethirtyeight.com/features/introducing-nfl-elo-ratings/
 df['Probability'] = 1 / (1+10**(-(df['WhiteElo'] - df['BlackElo'])/400))
 
-
-print(df['Probability'])
-
 df.Result2 = df.Result.apply(lambda val : 1 if val == '1-0' else (0 if val == '0-1' else 0.5))
 
-#print(df.probability)
-#print(df.Result2.mean())
-print(df.Result2.mean(), df.Probability.mean())
 
-print(df.Result, df.Probability)
-#print(df.Probability)
+#How often does an endgame arise?
+endgame_cnt = df[df['Pieces']<=16].Pieces.count()
+print("Share of endgames %3.2f" %  (endgame_cnt/df.shape[0]))
+
+#How many half-moves are played on average? 
+move_cnt = df.PlyCount.mean()
+print("Average Number of moves %3.2f" %  (move_cnt))
+
+#How well was the outcome predicted on average?
+print('We expected %3.2f points for white and got %3.2f' % (df.Probability.mean(), df.Result2.mean()) )
+
+
+#Do stronger players play more moves on average?
+print(df[(df['BlackElo'] > 2000) | (df['WhiteElo'] > 2000)].head)
+
+#How good is the performance of the stronger players if an endgame was reached?
+
+
