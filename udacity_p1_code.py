8515b49ec025cb4e5e7f7f9df3a0803fcfe13a42
t4mmger <tobias-ammon@gmx.de>
2023-04-26 18:55:03 +0200

    step2

--- udacity_p1_code.py
+++ udacity_p1_code.py
@@ -21,11 +21,11 @@ while True:
 headers = {'White': [], 'WhiteElo': [], 'Black': [], 'BlackElo': [], 'Result': [], 'ECO': [], 'PlyCount': [], 'Date': []}
 pieces = {'Pieces': []}
 for i, g in enumerate(my_games):
-    print(i)
+    #print(i)
     # this collects the headers information
     for el in headers: 
-        print(el)
-        print(g.headers[el])
+        #print(el)
+        #print(g.headers[el])
         headers[el] += [g.headers[el]]
     
     # this collects the number of pieces at the end of the game
@@ -41,10 +41,37 @@ df = pd.DataFrame(chess_df)
 
 df[['WhiteElo','BlackElo']] = df[['WhiteElo', 'BlackElo']].apply(pd.to_numeric)
 
+
+
+#https://stackoverflow.com/questions/34962104/how-can-i-use-the-apply-function-for-a-single-column
+#def elo_prob(w,b):
+    #print('W ist ', w, '. B ist ', b)
+    #if w >= b:
+    #    print('wir sind im if, we return ', 1 / (1+10**((w - b)/400) ))
+    #return 1 / (1+10**(-(w - b)/400))
+    #    
+    #else:
+    #    print('wir sind im else')
+    #    return 1 / (1+10**(-(w - b)/400))
+
+#def apply_elo_prob(x):
+ #   return elo_prob(x['WhiteElo'],x['BlackElo'])
+
+#df['Probability'] = df.apply(apply_elo_prob, axis = 1)    
+
+
+
 #formula for probability from https://fivethirtyeight.com/features/introducing-nfl-elo-ratings/
-df['Probability'] = 1 / (1+10**((df['WhiteElo'] - df['BlackElo'])/400))
+df['Probability'] = 1 / (1+10**(-(df['WhiteElo'] - df['BlackElo'])/400))
 
-print(df)
-df.dtypes
 
-print(my_games[1])
+print(df['Probability'])
+
+df.Result2 = df.Result.apply(lambda val : 1 if val == '1-0' else (0 if val == '0-1' else 0.5))
+
+#print(df.probability)
+#print(df.Result2.mean())
+print(df.Result2.mean(), df.Probability.mean())
+
+print(df.Result, df.Probability)
+#print(df.Probability)
