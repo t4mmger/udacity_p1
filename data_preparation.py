ec7a08a726e24ee4068c4c87086957e39fe6b09d
t4mmger <tobias-ammon@gmx.de>
2023-04-28 01:04:42 +0200

    split in more files, started with graphics

---
+++ data_preparation.py
@@ -0,0 +1,78 @@
+
+#### This code does not run on its own. It should be called by udacity_p1_code.py. 
+
+#set path and thresholds
+pgn = open("C:\\Users\\TAMM\\Desktop\\udacity\\projekt1\\kem_mfrmitte_2023_.pgn", encoding= 'unicode_escape')
+
+
+my_games = []
+
+#from https://github.com/niklasf/python-chess/issues/446
+while True:
+    game = chess.pgn.read_game(pgn)
+    if game is None:
+        break  # end of file
+    my_games.append(game)
+####
+##, 'ECO': []
+
+headers = {'White': [], 'WhiteElo': [], 'Black': [], 'BlackElo': [], 'Result': [], 'PlyCount': [], 'Date': []}
+pieces = {'Pieces': []}
+for i, g in enumerate(my_games):
+
+    # this collects the headers information
+    for el in headers: 
+        headers[el] += [g.headers[el]]
+    #print(g)
+    # this collects the number of pieces at the end of the game
+    pieces['Pieces'] += [32 - str(g.mainline_moves()).count('x')]
+    #if i >= 10:
+     #   break
+
+chess_df = {**headers,**pieces}
+df = pd.DataFrame(chess_df)
+#print(df)
+df[['WhiteElo','BlackElo','Pieces','PlyCount']] = df[['WhiteElo', 'BlackElo','Pieces','PlyCount']].apply(pd.to_numeric)
+
+
+#formula for probability from https://fivethirtyeight.com/features/introducing-nfl-elo-ratings/
+df['Probability'] = 1 / (1+10**(-(df['WhiteElo'] - df['BlackElo'])/400))
+
+df['Result2'] = df.Result.apply(lambda val : 0.5 if val == '1-0' else (-0.5 if val == '0-1' else 0.0))
+
+#https://stackoverflow.com/questions/34962104/how-can-i-use-the-apply-function-for-a-single-column
+def result_change(w,b,r):
+    if w >= b:
+        return r
+    else:
+        return -r
+
+def apply_rc(x):
+    return result_change(x['WhiteElo'], x['BlackElo'], x['Result2'])
+
+
+def prob_change(w,b,p):
+    if w >= b:
+        return p
+    else:
+        return 1-p
+
+def apply_pc(x):
+    return prob_change(x['WhiteElo'], x['BlackElo'], x['Probability'])
+
+def color_funk(w,b):
+    if w >= b:
+        return 1
+    else:
+        return 0
+    
+def apply_cf(x):
+    return color_funk(x['WhiteElo'], x['BlackElo'])
+    
+df['Strong_Player_Result'] = df.apply(apply_rc, axis=1)
+df['Strong_Player_Prob'] = df.apply(apply_pc, axis=1)
+df['Strong_Player_White'] = df.apply(apply_cf, axis=1)
+df['Elo_Dif'] = abs(df.WhiteElo - df.BlackElo)
+
+print('Fertig!')
+# %%
--- udacity_p1_code.py
+++ udacity_p1_code.py
@@ -1,82 +1,106 @@
 ## chess project
 
-import pandas as pd
+#%%
+
 import chess 
 import chess.pgn 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import statsmodels.api as sma
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn import preprocessing
 
-pgn = open("C:\\Users\\TAMM\\Desktop\\udacity\\projekt1\\kem_mfrmitte_2023.pgn")
+endgame_threshold = 16
 
-my_games = []
-
-
-#from https://github.com/niklasf/python-chess/issues/446
-while True:
-    game = chess.pgn.read_game(pgn)
-    if game is None:
-        break  # end of file
-    my_games.append(game)
-####
-
-
-headers = {'White': [], 'WhiteElo': [], 'Black': [], 'BlackElo': [], 'Result': [], 'ECO': [], 'PlyCount': [], 'Date': []}
-pieces = {'Pieces': []}
-for i, g in enumerate(my_games):
-
-    # this collects the headers information
-    for el in headers: 
-        headers[el] += [g.headers[el]]
-    
-    # this collects the number of pieces at the end of the game
-    pieces['Pieces'] += [32 - str(g.mainline_moves()).count('x')]
-    if i >= 5:
-        break
-
-chess_df = {**headers,**pieces}
-
-df = pd.DataFrame(chess_df)
-
-df[['WhiteElo','BlackElo','Pieces','PlyCount']] = df[['WhiteElo', 'BlackElo','Pieces','PlyCount']].apply(pd.to_numeric)
+#%% [markdown]
+# ## this is markdown.
+# smaller text
+print('hallo!')
 
 
+#%% 
+#data preparation
+exec(open("C:\\Users\\TAMM\\Desktop\\udacity\\projekt1\\udacity_p1\\data_preparation.py").read())
 
-#https://stackoverflow.com/questions/34962104/how-can-i-use-the-apply-function-for-a-single-column
-#def elo_prob(w,b):
-    #print('W ist ', w, '. B ist ', b)
-    #if w >= b:
-    #    print('wir sind im if, we return ', 1 / (1+10**((w - b)/400) ))
-    #return 1 / (1+10**(-(w - b)/400))
-    #    
-    #else:
-    #    print('wir sind im else')
-    #    return 1 / (1+10**(-(w - b)/400))
+df = df 
 
-#def apply_elo_prob(x):
- #   return elo_prob(x['WhiteElo'],x['BlackElo'])
+#Graphics
 
-#df['Probability'] = df.apply(apply_elo_prob, axis = 1)    
+#%% 
+#sns.barplot(data = df, x=df['Result'], y=df['WhiteElo'], color = 'Blue' )
+#plt.show()
+
+sns.countplot(data = df, x=df['Result'], palette=  ['Black', 'Grey'], order=['1-0', '1/2-1/2', '0-1'], hue = df.Strong_Player_White )
+plt.show()
+
+sns.countplot(data = df, x=df['PlyCount'], color = 'Blue')
+plt.show()
+#sns.barplot(data = df, y=df.PlyCount.value_counts(), x=df['PlyCount'], color='Blue')
+#plt.show()
 
 
-#formula for probability from https://fivethirtyeight.com/features/introducing-nfl-elo-ratings/
-df['Probability'] = 1 / (1+10**(-(df['WhiteElo'] - df['BlackElo'])/400))
+#%%
+##############
+#General statistics
 
-df.Result2 = df.Result.apply(lambda val : 1 if val == '1-0' else (0 if val == '0-1' else 0.5))
-
+#How many games do we have? 
+print("Number of games: ",  df.shape[0])
 
 #How often does an endgame arise?
-endgame_cnt = df[df['Pieces']<=16].Pieces.count()
+endgame_cnt = df[df['Pieces']<=endgame_threshold].Pieces.count()
 print("Share of endgames %3.2f" %  (endgame_cnt/df.shape[0]))
 
 #How many half-moves are played on average? 
 move_cnt = df.PlyCount.mean()
-print("Average Number of moves %3.2f" %  (move_cnt))
+print("Average Number of half-moves %3.2f" %  (move_cnt))
 
 #How well was the outcome predicted on average?
-print('We expected %3.2f points for white and got %3.2f' % (df.Probability.mean(), df.Result2.mean()) )
+print('We expected %3.2f points for white and got %3.2f' % (df.Probability.mean(), df.Result2.mean()+0.5) )
 
 
 #Do stronger players play more moves on average?
-print(df[(df['BlackElo'] > 2000) | (df['WhiteElo'] > 2000)].head)
+print('Average number of moves for')
+print('Stronger players: ', df[(df['BlackElo'] > 2000) | (df['WhiteElo'] > 2000)].PlyCount.mean())
+print('Weaker players: ', df[(df['BlackElo'] <= 2000) | (df['WhiteElo'] <= 2000)].PlyCount.mean())
 
-#How good is the performance of the stronger players if an endgame was reached?
+
+#Create dataset strong players vs weak players
+strong_weak = df[((df['BlackElo'] <= 2000) & (df['WhiteElo'] > 2000)) | ((df['BlackElo'] > 2000) & (df['WhiteElo'] <= 2000))]
+#print(strong_weak)
+
+sns.barplot(data = df, x=df['Strong_Player_Result'], y=df['PlyCount'])
+plt.show()
+
+##############
+#How good is the performance of the stronger players vs. weaker players?
+
+result = strong_weak.Strong_Player_Result.mean()+.5
+prob = strong_weak.Strong_Player_Prob.mean()
+count = strong_weak.shape[0]
+
+print('Number of games with strong vs weak players: ', count)
+print('Avg. Points for stronger players really: ', result)
+print('Avg. Points for stronger players expected: ', prob)
 
 
+##############
+#How good is the performance of the stronger players if an endgame was reached vs. weaker players?
+
+result  = strong_weak[strong_weak['Pieces'] <= endgame_threshold].Strong_Player_Result.mean()+.5
+prob = strong_weak[strong_weak['Pieces'] <= endgame_threshold].Strong_Player_Prob.mean()
+count = strong_weak[strong_weak['Pieces'] <= endgame_threshold].shape[0]
+
+print('Number of endgames with strong vs weak players: ', count)
+print('Avg. Points for stronger players in endgame really: ', result)
+print('Avg. Points for stronger players in endgame expected: ', prob)
+
+
+#regression
+#exec(open("C:\\Users\\TAMM\\Desktop\\udacity\\projekt1\\udacity_p1\\regression.py").read())
+
+
+# %%
