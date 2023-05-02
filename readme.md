0b1866e45aa1df81bf9fc39ebc0aa3a2038e6698
t4mmger <tobias-ammon@gmx.de>
2023-05-02 14:23:47 +0200

    pretty much final.

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 
-### 1. Installations:
+# 1. Installations:
 
 import chess 
 import chess.pgn 
@@ -16,31 +16,35 @@ from sklearn.metrics import r2_score, mean_squared_error
 from sklearn import preprocessing
 from statsmodels.discrete.discrete_model import Probit
 
-### 2. Project Motivations
+# 2. Project Motivations
 
 In this project I analyse chess games with players rated between 2000 and 2500 elo rating. I do some statistics on the complete dataset before answering the following questions:
 
 Is there a difference of playing style between players with Elo >= 2250 and players with Elo < 2250? How often do they reach endgames and how many moves to they play? Is the probability for a point suggested by Elo still valid when we enter the endgame phase? 
-# Are there differences in play between weak and strong players?
+### Are there differences in play between weak and strong players?
 
-If strong players play vs weaker, do we see different patterns in the game? E.g. longer games because the stronger side tries harder to win? Games that take longer until they become endgames because the stronger players try to keep more pieces on the board to outplay the opponent? 
-# Are game staticts different when strong players play against weaker players? 
+If strong players play vs weaker, do we see different patterns in the game? E.g. longer games because the stronger side tries harder to win? 
+### Are game statistics different when strong players play against weaker players? 
 
 
 The Elo-difference gives a probability how many points the stronger person is expected to score. The color is not taken into account. I run a regression to check answer this question:
-# Is the color White still a factor?
+### Is the color White still a factor?
 
 This analyses could prove useful for someone who would raise his rating from 2000 to > 2250. With some modifications the usefulness can be widen to a broader group. 
 
-### 3. File Descsription
+# 3. File Descsription
 
-pgn file - 
-code file - 
-readme file - 
+pgn file: 1980_1989.pgn a sample of chess games from 1980-1989 with ratings between 2000 and 2500. \
+csv file: game_result_list.csv, selected information from the pgn file and also some cleaning was done. see data_preparation.py for details. \
+py files: data_preparation.py the data steps to transform the pgn into csv. \ udacity_p1_code.py main file with import of modules, statistics and regression.
+readme file: 
 
-### 4. Technical details:
+# 4. Technical details:
 
 I use Python 3.10.9, packaged by Anaconda Inc. 
 
-### 5. Licensing, Authors, Acknowledgements 
+# 5. Licensing, Authors, Acknowledgements 
 
+Author: t4mmger
+Acknowledgements: to all who contribute to the python community.
+Licensing: free
--- udacity_p1_code.py
+++ udacity_p1_code.py
@@ -16,6 +16,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
 from sklearn import preprocessing
 from statsmodels.discrete.discrete_model import Probit
+
 endgame_threshold = 16
 elo_threshold = 2250
 
@@ -25,30 +26,55 @@ elo_threshold = 2250
 ### Introduction
 
 # What this analysis is about.
-# Some words on the dataset. 
+
+## Dataset
+# I use a pgn file with roughly 140k games from players rated between 2000 and 2500 and the years 1980-1989. I chose this period because I wanted to have over the board games with longer time control. 
+#
+# The rating is not always Elo and sometimes the import does not work. I handle the first by dropping non elo games and the second by working with try and except.
+#
+# I also delete some duplicates. In the end I have still around 110k games.
 
 
 #%% 
-#Data Preparation
+#Data Import/Preparation
 
 #This takes some time. You don't have to run it, but import the csv file. 
 #exec(open("C:\\Users\\TAMM\\Desktop\\udacity\\projekt1\\udacity_p1\\data_preparation.py").read())
 
 df = pd.read_csv(filepath_or_buffer = "C:\\Users\\TAMM\\Desktop\\udacity\\projekt1\\udacity_p1\\game_result_list.csv", sep = ';')
 
-#%% 
+#%%
 
 sns.countplot(data = df, x=df['Result'], palette = ['Black', 'Grey'], order=['1-0', '1/2-1/2', '0-1'], hue = df.Strong_Player_White )
 plt.show()
 
-#sns.countplot(data = df, x=df['Strong_Player_Result'], hue = df.Strong_Player_White )
-#plt.show()
-
-sns.kdeplot(data=df, x=df['PlyCount'], legend = False, hue= df['Pieces'] <= endgame_threshold, shade=True)
+sns.kdeplot(data=df, x=df['PlyCount'], legend = False, hue= df['Pieces'] <= endgame_threshold, fill=True)
 plt.title('How many moves were played and was an endgame reached?')
 plt.legend(title = 'Endgame reached' , loc='upper right', labels=['Yes', 'No'])
+plt.xticks(ticks = [0,20,40,60,80,100,120,140,160, 180, 200])
 plt.xlim(0,200)
 plt.show()
+
+sns.kdeplot(data=df, x=df['PlyCount'], legend = True, hue= df['Result'], fill=True)
+plt.title('How many moves were played and how did the game end?')
+plt.xticks(ticks = [0,20,40,60,80,100,120,140,160, 180, 200])
+plt.xlim(0,200)
+plt.show()
+
+#%% [markdown]
+
+# The stronger player have more often White in my dataset and it is much easier to beat them when one plays White against them. 
+#
+# When the game does not reach an endgame most games end around move 25 (PlyCount 50). Whether by a successful tactical skirmish or by draw we cannot see from figure 2.
+# 
+# Around PlyCount 50 the number of games ending in an Endgame rises fast and reaching the maximum at around 80. I had to restrict the x-Axis to <= 200 to remove outliers. 
+# 
+# We see that endgames occure quite frequently and can take some time until the game ends. Thus endgame skills are needed for every ambitious chess player. 
+#
+#The third figure shows the first peak of draws around PlyCount 25-30, early draws directly after the opening. Another draw peak is after move 40. 
+# 
+# For decisive games it is different: the peak is before move 40. After move 40 usually the players get more time. Consequently they have little time before it and so errors happen more frequently. After move 40 the players take a fresh look at the position and convince themselves that a draw is not so bad. 
+
 
 #%% [markdown]
 ##############
@@ -56,40 +82,69 @@ plt.show()
 
 print(df.describe())
 
-#How many games do we have? 
-print("Number of games: ",  df.shape[0])
+#%% [markdown]
+# We have 109623 games, and the White Elo is slightly better than Black's.\ 
+# We have some games which ended already after 1 moves. This is mostly because the other player did not show up. \
+# The median of PlyCount is 74,  which is about the mean, too. \
+# On average the games ended with a little more than 16 pieces on the board, so over our Endgame Threshold.\
+# The probability of White scoring is 0.505, because White's Elo a little bit higher.\
+# The actual result was even better with 0.555 (We have to add 0.5 to Result2), indicating that the White pieces have an advantage.
+
+#%%
+
+############## ENDGAME
 
 #How often does an endgame arise?
 endgame_cnt = df[df['Pieces']<=endgame_threshold].Pieces.count()
 print("Share of endgames %3.2f" %  (endgame_cnt/df.shape[0]))
 
-#How many half-moves are played on average? 
+#Do stronger players play reach endgames more often on average?
+endgame_cnt1 = df[((df['Pieces']<=endgame_threshold) & ((df['BlackElo'] > elo_threshold) | (df['WhiteElo'] > elo_threshold)))].Pieces.count()
+print("How often do stronger players reach an endgame? %3.2f" %  (endgame_cnt1/df[((df['BlackElo'] > elo_threshold) | (df['WhiteElo'] > elo_threshold))].shape[0]))
+
+endgame_cnt2 = df[((df['Pieces']<=endgame_threshold) & ((df['BlackElo'] <= elo_threshold) | (df['WhiteElo'] <= elo_threshold)))].Pieces.count()
+print("How often do weaker players reach an endgame? %3.2f" %  (endgame_cnt2/df[((df['BlackElo'] <= elo_threshold) | (df['WhiteElo'] <= elo_threshold))].shape[0]))
+
+#%% [markdown]
+#We do not notice any difference between on Endgames stronger and weaker players. 
+
+#%%
+############## MOVES
+
+#How many half-moves are played on average?
 move_cnt = df.PlyCount.mean()
 print("Average Number of half-moves %3.2f" %  (move_cnt))
 
-#How well was the outcome predicted on average?
-print('We expected %3.2f points for white and got %3.2f' % (df.Probability.mean(), df.Result2.mean()+0.5) )
-
 #Do stronger players play more moves on average?
-print('Average number of moves for')
+print('Average number of half-moves for')
 print('Stronger players: ', df[(df['BlackElo'] > elo_threshold) | (df['WhiteElo'] > elo_threshold)].PlyCount.mean())
 print('Weaker players: ', df[(df['BlackElo'] <= elo_threshold) | (df['WhiteElo'] <= elo_threshold)].PlyCount.mean())
 
+#%% [markdown]
+#We do neither notice any difference on moves between stronger and weaker players. 
 
+
+#%% [markdown]
+### Are game statistics different when strong players play against weaker players? 
+
+#%%
+############## STRONG VS WEAK
 #Create dataset strong players vs weak players
 strong_weak = df[((df['BlackElo'] <= elo_threshold) & (df['WhiteElo'] > elo_threshold)) | ((df['BlackElo'] > elo_threshold) & (df['WhiteElo'] <= elo_threshold))]
 
-##############
+#How many moves do the games have?
+print('Average half-moves in dataset: %5.3f' % (df.PlyCount.mean()))
+print('Average half-moves when strong players face weak players: %5.3f' % (strong_weak.PlyCount.mean()))
+
 #How good is the performance of the stronger players vs. weaker players?
 
 result = strong_weak.Strong_Player_Result.mean()
 prob = strong_weak.Strong_Player_Prob.mean()
 count = strong_weak.shape[0]
 
-print('Number of games with strong vs weak players: ', count)
-print('Avg. Points for stronger players really: ', result)
-print('Avg. Points for stronger players expected: ', prob)
-
+print('Number of games with strong vs weak players: ', count )
+print('Avg. Points for stronger players really: %5.4f' % result)
+print('Avg. Points for stronger players expected: %5.4f' % prob)
 
 ##############
 #How good is the performance of the stronger players if an endgame was reached vs. weaker players?
@@ -99,22 +154,61 @@ prob = strong_weak[strong_weak['Pieces'] <= endgame_threshold].Strong_Player_Pro
 count = strong_weak[strong_weak['Pieces'] <= endgame_threshold].shape[0]
 
 print('Number of endgames with strong vs weak players: ', count)
-print('Avg. Points for stronger players in endgame really: ', result)
-print('Avg. Points for stronger players in endgame expected: ', prob)
+print('Avg. Points for stronger players in endgame really: %5.4f' % result)
+print('Avg. Points for stronger players in endgame expected: %5.4f' % prob)
+
+#%% [markdown]
+# The game length does not vary a lot. 
+# When stronger players play weaker players they tend to score less points than expected, no matter if the games ends in an Endgame or not.
+# Do you have an idea why this is so?
 
 #%% [markdown]
 # 
 ### Is White important for the win probability? 
 # We will answer this question with a OLS regression. The y variable will be the result from the stronger player's view.
+#
 # Our X-variables will be the Elo Difference and a White-Dummy.
+#
+#How well was the outcome predicted by Elo on average?
+print('Based on Elo Probability we expected %3.2f points for white and got %3.2f' % (df.Probability.mean(), df.Result2.mean()+0.5) )
 
-exec(open("C:\\Users\\TAMM\\Desktop\\udacity\\projekt1\\udacity_p1\\regression.py").read())
+#Regression part
+
+#Split into train and test
+
+X = df[['Elo_Dif', 'Strong_Player_White']]
+y = df['Strong_Player_Result']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .30, random_state=42)
+
+lm_model = LinearRegression(fit_intercept=True #normalize = True
+    ) # Instantiate
+
+lm_model.fit(X_train, y_train) #Fit
+
+y_test_preds = lm_model.predict(X_test)
+y_train_preds = lm_model.predict(X_train)
+
+test_score = r2_score(y_test, y_test_preds)
+train_score = r2_score(y_train, y_train_preds)
+print('R^2 Score is on our test sample is: %5.4f' % (test_score))
+print('In Training we had: %5.4f' % (train_score))
+
+#print('Mean predictions and real mean on traing set:  %5.4f vs %5.4f' %  (y_train_preds.mean(), y_train.mean()))
+print('Mean predictions and real mean on test set: %5.4f vs %5.4f' % ( y_test_preds.mean(), y_test.mean()))
+
+#From https://www.cluzters.ai/forums/topic/395/find-p-value-significance-in-scikit-learn-linear-regression?c=1597
+
+X_train2 = sma.add_constant(X_train)
+est = sma.OLS(y_train, X_train2)
+est2 = est.fit()
+print(est2.summary())
+
 
 #%%[markdown] Indeed the white pieces are favorable. 
 
 ## How can we improve things?
 #  We only did an OLS regression, but the win probability based on Elo difference follows a normal or logit distribution, so a logit or probit regression would be better.
 
-# What do you think is most important for improving your chess?
+# What would you suggest to others on how they can improve in chess?
 
 # %%
